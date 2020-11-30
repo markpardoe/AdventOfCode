2f7fa4e677a50f706113341755b7cd51dd7a7254
Mark <markpardoe@gmail.com>
2020-11-30 19:46:55 +0000

    Add project template for AdventOfCode 2020

---
+++ 2020/AcC2020/Problems/Day01/Day01_Solution.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using AoC.Common;
+
+namespace AoC.AoC2020.Problems.Day01
+{
+    class Day01_Solution : AoCSolution
+    {
+
+        public override int Year => 2020;
+        public override int Day => 1;
+        public override string Name => "Day 01: ";
+        public override string InputFileName => null;
+
+        public override IEnumerable<string> Solve(IEnumerable<string> input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+
+}
---
+++ 2020/AcC2020/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using AoC.Common;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+using AoC.AoC2020.Problems.Day01;
+
+namespace AoC.AoC2020
+{
+    class Program
+    {
+        static void Main()
+        {
+            AoCSolution problem = new Problems.Day01.Day01_Solution();
+            List<string> data = new List<string>();
+            if (problem.InputFileName != null)
+            {
+                string inputFile = Path.Combine(System.Environment.CurrentDirectory, "InputData", problem.InputFileName);
+                data.AddRange(File.ReadAllLines(inputFile).ToList());
+            }
+
+            Console.WriteLine(problem.Name);
+
+            foreach (var result in problem.Solve(data))
+            {
+                Console.WriteLine(result);
+            }
+
+            Console.WriteLine("Press enter to exit.");
+            Console.ReadLine();
+        }
+    }
+}
