feba376838114eb82fabdf5cbf6a126e4622d02b
Mark <markpardoe@gmail.com>
2020-12-01 09:03:43 +0000

    Add solution for day 1

--- 2020/AcC2020/Problems/Day01/Day01_Solution.cs
+++ 2020/AcC2020/Problems/Day01/Day01_Solution.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using AoC.Common;
 
@@ -10,14 +11,72 @@ namespace AoC.AoC2020.Problems.Day01
 
         public override int Year => 2020;
         public override int Day => 1;
-        public override string Name => "Day 01: ";
-        public override string InputFileName => null;
+        public override string Name => "Day 1: Report Repair";
+        public override string InputFileName => "Day01.txt";
 
         public override IEnumerable<string> Solve(IEnumerable<string> input)
         {
-            throw new NotImplementedException();
+            yield return RepairReport(2020, input).ToString();
+            yield return RepairReportThreeNumbers(2020, input).ToString();
+        }
+
+        public int RepairReport(int target, IEnumerable<string> input)
+        {
+            var data = input.Select(x => int.Parse(x)).ToList();
+            data.Sort();
+
+            for (int i = 0; i < data.Count; i++)
+            {
+                for (int j = i+1; j < data.Count;j++)
+                {
+                    int currentValue = data[i] + data[j];
+                    if (currentValue == target)
+                    {
+                        return data[i] * data[j];
+                    }
+                    else if (currentValue > target)
+                    {
+                        break;
+                    }
+                }
+            }
+            return -1;
+        }
+
+
+        public int RepairReportThreeNumbers(int target, IEnumerable<string> input)
+        {
+            var data = input.Select(x => int.Parse(x)).ToList();
+            data.Sort();
+         //   Console.WriteLine($"Data Length = {data.Count}");
+
+            for (int i = 0; i < data.Count; i++)
+            {
+                for (int j = i + 1; j < data.Count; j++)
+                {
+                   
+                    if (data[i] + data[j] >= target)
+                    {
+                        break;
+                    }
+
+                    for (int k = j + 1; k < data.Count; k++)
+                    { 
+                        int currentValue = data[i] + data[j] + data[k];
+                        if (currentValue == target)
+                        {
+                            //Console.WriteLine($"i: {i} = {data[i]}, j: {j}  = {data[j]}, k: {k}  = {data[k]} = {currentValue}");
+                            return data[i] * data[j] * data[k];
+                        }
+
+                        if (currentValue >= target)
+                        {
+                            break;
+                        }
+                    }
+                }
+            }
+            return -1;
         }
     }
-
-
 }
