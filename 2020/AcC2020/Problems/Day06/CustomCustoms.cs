df613f5acd1d668b26b3c719ce7dc0af571af41d
Mark <markpardoe@gmail.com>
2020-12-06 19:17:21 +0000

    Add Day 6 2020

---
+++ 2020/AcC2020/Problems/Day06/CustomCustoms.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AoC.Common;
+using AoC.Common.DataStructures;
+
+namespace AoC.AoC2020.Problems.Day06
+{
+    public class CustomCustoms : AoCSolution<int>
+    {
+        public override IEnumerable<int> Solve(IEnumerable<string> input)
+        {
+            int totalAnyAnswer = 0; // answer for part A
+            int totalAllAnswers = 0; // answer for part B
+            var group = new List<string>();
+
+            // Process input file
+            foreach (string line in input)
+            {
+                if (string.IsNullOrEmpty(line) && group.Count > 0)
+                {
+                    // Empty line indicates a new group.
+                    // So get total answers for previous group - then start a new grouping.
+                    var answers = CountAnswers(group);
+                    totalAnyAnswer += answers.Keys.Count;
+                    totalAllAnswers += answers.Values.Count(v => v == group.Count);
+
+                    group = new List<string>();
+                }
+                else
+                {
+                    group.Add(line);
+                }
+            }
+
+            if (group.Count > 0)
+            {
+                var answers = CountAnswers(group);
+                totalAnyAnswer += answers.Keys.Count;
+                totalAllAnswers += answers.Values.Count(v => v == group.Count);
+            }
+
+            yield return totalAnyAnswer;
+            yield return totalAllAnswers;
+        }
+
+        public override int Year => 2020;
+        public override int Day => 6;
+        public override string Name => "Day 6: Custom Customs";
+        public override string InputFileName => "Day06.txt";
+
+        public ItemCounter<char> CountAnswers(IEnumerable<string> data)
+        {
+            var answers = new ItemCounter<char>();
+            int lineCount = 0;
+            foreach (string line in data)
+            {
+                if (!string.IsNullOrEmpty(line))
+                {
+                    lineCount++;
+                    foreach (char c in line)
+                    {
+                        answers.Add(c);
+                    }
+                }
+            }
+            return answers;
+        }
+    }
+}
---
+++ Common/DataStructures/ItemCounter.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AoC.Common.DataStructures
+{
+
+    /// <summary>
+    /// Used to count the number of instances of a value that are added to the collection.
+    /// Adding an item increments the number of items by 1.
+    /// Removing decreases number of items by 1 - if total <=0 then the item is removed completely.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class ItemCounter<T> :ICollection<T>
+    {
+        private readonly Dictionary<T, long> _internalDictionary = new Dictionary<T, long>();
+
+
+        public bool Remove(T item)
+        {
+            if (_internalDictionary.ContainsKey(item))
+            {
+                var value = _internalDictionary[item] - 1;
+                if (value <= 0)
+                {
+                    _internalDictionary.Remove(item);
+                }
+                else
+                {
+                    _internalDictionary[item] = value;
+                }
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public int Count => _internalDictionary.Count;
+        public bool IsReadOnly => false;
+
+
+        public void Clear()
+        {
+            _internalDictionary.Clear();
+        }
+
+        public bool Contains(T item)
+        {
+            return _internalDictionary.ContainsKey(item);
+        }
+
+        public void CopyTo(T[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return _internalDictionary.Keys.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        public void Add(T item)
+        {
+            // item found
+            if (_internalDictionary.ContainsKey(item))
+            {
+                _internalDictionary[item] = _internalDictionary[item] + 1;
+            }
+            else
+            {
+                _internalDictionary[item] = 1;
+            }
+        }
+
+        public long this[T item]
+        {
+            get
+            {
+                if (!_internalDictionary.ContainsKey(item))
+                {
+                    return 0;
+                }
+                else
+                {
+                    return _internalDictionary[item];
+                }
+            }
+        }
+
+        public Dictionary<T, long>.KeyCollection Keys => _internalDictionary.Keys;
+        public Dictionary<T, long>.ValueCollection Values => _internalDictionary.Values;
+
+
+        public long Sum => _internalDictionary.Values.Sum();
+
+    }
+}
