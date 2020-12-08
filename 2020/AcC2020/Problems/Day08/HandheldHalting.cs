67f2efd027d15c547124703b2778f50600573510
Mark <markpardoe@gmail.com>
2020-12-08 11:42:39 +0000

    Add 2020 Day 08 part 2

--- 2020/AcC2020/Problems/Day08/HandheldHalting.cs
+++ 2020/AcC2020/Problems/Day08/HandheldHalting.cs
@@ -1,5 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using AoC.Common;
 
@@ -9,10 +12,11 @@ namespace AoC.AoC2020.Problems.Day08
     {
         public override IEnumerable<int> Solve(IEnumerable<string> input)
         {
-            ProgramState state = new ProgramState(input);
+            var instructions = ProgramState.GenerateInstructions(input);
+            ProgramState state = new ProgramState(instructions);
             yield return FindValueAtRepeat(state);
-
-           // throw new NotImplementedException();
+            yield return FixProgram(instructions);
+            // throw new NotImplementedException();
         }
 
         public override int Year => 2020;
@@ -24,8 +28,6 @@ namespace AoC.AoC2020.Problems.Day08
         private int FindValueAtRepeat(ProgramState state)
         {
             HashSet<int> checkedInstructions = new HashSet<int>();
-
-
             while (!checkedInstructions.Contains(state.Index))
             {
                 checkedInstructions.Add(state.Index);
@@ -33,6 +35,63 @@ namespace AoC.AoC2020.Problems.Day08
             }
 
             return state.Accumulator;
+        }
+
+
+        private int FixProgram(IEnumerable<Instruction> input)
+        {
+            List<Instruction> instructions = input.ToList();
+
+            for (int i = 0; i < instructions.Count; i++)
+            {
+                Console.WriteLine($"Checking line: {i}");
+                Instruction originalInstruction = instructions[i];
+                if (originalInstruction.Type != InstructionType.Accumulate)
+                {
+                    Instruction newInstruction = null;
+
+                    if (originalInstruction.Type == InstructionType.Jump)
+                    {
+                        newInstruction = new Instruction(InstructionType.NoOperation, originalInstruction.Value);
+                    }
+                    else if (originalInstruction.Type == InstructionType.NoOperation)
+                    {
+                        newInstruction = new Instruction(InstructionType.Jump, originalInstruction.Value);
+                    }
+                    else
+                    {
+                        throw new InvalidDataException();
+                    }
+
+                    instructions[i] = newInstruction;
+                    var newState = new ProgramState(instructions);
+                    if (IsProgramComplete(newState))
+                    {
+                        return newState.Accumulator;
+                    }
+                    else
+                    {
+                        // put the old instruction back...
+                        instructions[i] = originalInstruction;
+                    }
+                }
+            }
+
+            return -1;
+        }
+
+        // Check if a given program state - does it finish by moving past the final instruction
+        private bool IsProgramComplete(ProgramState state)
+        {
+            HashSet<int> checkedInstructions = new HashSet<int>();
+            while (!checkedInstructions.Contains(state.Index) && state.Status == ProgramStatus.Running)
+            {
+                checkedInstructions.Add(state.Index);
+                state.ExecuteNextInstruction();
+            }
+
+            return state.Status == ProgramStatus.Complete;
+
         }
     }
 
--- 2020/AcC2020/Problems/Day08/ProgramState.cs
+++ 2020/AcC2020/Problems/Day08/ProgramState.cs
@@ -1,10 +1,17 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 
 namespace AoC.AoC2020.Problems.Day08
 {
+    public enum ProgramStatus
+    {
+        Running,
+        Complete
+    }
+
     public class ProgramState
     {
-        private readonly Dictionary<string, InstructionType> _instructionTypesMapper = new Dictionary<string, InstructionType>
+        private static readonly Dictionary<string, InstructionType> _instructionTypesMapper = new Dictionary<string, InstructionType>
         {
             {"acc", InstructionType.Accumulate},
             {"jmp", InstructionType.Jump},
@@ -13,38 +20,64 @@ namespace AoC.AoC2020.Problems.Day08
 
         public int Accumulator { get; private set; } = 0;
         public int Index { get; private set; } = 0;
+        public ProgramStatus Status { get; private set; } = ProgramStatus.Running;
 
         private List<Instruction> _instructions = new List<Instruction>();
 
-        public ProgramState(IEnumerable<string> input)
+        public ProgramState(IEnumerable<string> input) : this(ProgramState.GenerateInstructions(input))
         {
-            foreach (string line in input)
-            {
-                var c = line.Split(' ');
-                _instructions.Add(new Instruction(_instructionTypesMapper[c[0]], int.Parse(c[1])));
-            }
+        }
+
+        public ProgramState(IEnumerable<Instruction> instructions)
+        {
+            // use ToList to make a copy
+            _instructions = instructions.ToList();  
         }
 
         public void ExecuteNextInstruction()
         {
-            var instruction = _instructions[Index];
+            if (Index >= _instructions.Count || Status == ProgramStatus.Complete)
+            {
+                Status = ProgramStatus.Complete;
+            }
+            else
+            {
+                var instruction = _instructions[Index];
 
-            // Should move these to seperate classes (eg. public class JumpInstruction : Instruction {})
-            // but no need if these are only used for this one problem.
-            // If new instructions are added later, we can revisit this.
-            if (instruction.Type == InstructionType.Accumulate)
-            {
-                Accumulator += instruction.Value;
-                Index++;
+                // Should move these to seperate classes (eg. public class JumpInstruction : Instruction {})
+                // but no need if these are only used for this one problem.
+                // If new instructions are added later, we can revisit this.
+                if (instruction.Type == InstructionType.Accumulate)
+                {
+                    Accumulator += instruction.Value;
+                    Index++;
+                }
+                else if (instruction.Type == InstructionType.Jump)
+                {
+                    Index += instruction.Value;
+                }
+                else if (instruction.Type == InstructionType.NoOperation)
+                {
+                    Index++;
+                }
+
+                if (Index >= _instructions.Count)
+                {
+                    Status = ProgramStatus.Complete;
+                }
             }
-            else if (instruction.Type == InstructionType.Jump)
+        }
+
+        public static IList<Instruction> GenerateInstructions(IEnumerable<string> input)
+        {
+            List<Instruction> instructions = new List<Instruction>();
+            foreach (string line in input)
             {
-                Index += instruction.Value;
+                var c = line.Split(' ');
+                instructions.Add(new Instruction(_instructionTypesMapper[c[0]], int.Parse(c[1])));
             }
-            else if (instruction.Type == InstructionType.NoOperation)
-            {
-                Index++;
-            }
+
+            return instructions;
         }
     }
 }
