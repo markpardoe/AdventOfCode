995eac4f3f4dabb9b54afb696e9aa2d861f25633
Mark <markpardoe@gmail.com>
2020-12-15 10:55:06 +0000

    Add 2020 Day 15 solution

---
+++ 2020/AcC2020/Problems/Day15/RambunctiousRecitation.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using AoC.Common;
+
+namespace AoC.AoC2020.Problems.Day15
+{
+    public class RambunctiousRecitation :AoCSolution<int>
+    {
+        public override int Year => 2020;
+        public override int Day => 15;
+        public override string Name => "Day 15: Rambunctious Recitation";
+        public override string InputFileName => null;
+
+        private readonly List<int> PuzzleInput = new List<int>() {0, 14, 6, 20, 1, 4};
+
+        private readonly List<int> Example1 = new List<int>() { 0, 3, 6 };
+
+        public override IEnumerable<int> Solve(IEnumerable<string> input)
+        {
+            NumberGame game = new NumberGame(PuzzleInput);
+
+
+            // Brute force the answer to part B.  Not ideal, but runs in approx. 10 seconds
+            while (game.Turn <= 30000000)
+            {
+                int value = game.TakeTurn();
+                if (game.Turn == 2020)
+                {
+                    yield return game.LastNumber;
+                }
+            }
+
+            yield return game.LastNumber;
+        }
+    }
+
+
+    public class NumberGame
+    {
+        public int Turn { get; private set; } = 1;
+
+        // Holds the turn(s) in which each number was spoken
+        private readonly Dictionary<int, List<int>> _previousNumbers = new Dictionary<int, List<int>>();
+        private readonly List<int> _initialTurns;
+        private readonly List<int> _numbers = new List<int>();
+
+        public int LastNumber => _numbers.Last();
+       
+        public NumberGame(List<int> initialValues)
+        {
+            _initialTurns = initialValues;
+        }
+
+        /// <summary>
+        /// Add a turn to the cached list of turns per value
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="turn"></param>
+        public void AddNumber(int value, int turn)
+        {
+            if (!_previousNumbers.ContainsKey(value))
+            {
+                _previousNumbers.Add(value, new List<int>());
+            }
+
+            _previousNumbers[value].Add(turn);
+        }
+
+        public int TakeTurn()
+        {
+            // either take number from initial list, or calculate it from the last number spoken
+            int value = Turn <= _initialTurns.Count ? _initialTurns[Turn - 1] : CalculateNumber(_numbers.Last());
+
+            AddNumber(value, Turn);
+            _numbers.Add(value);
+            Turn++;
+            return value;
+        }
+
+
+        // Returns how many times this number has been said
+        private int CalculateNumber(int lastNumber)
+        {
+            var turns = _previousNumbers[lastNumber];
+            int count = turns.Count;
+            if (count == 1)
+            {
+                return 0;  // this number has either been said before, or only said once
+            }
+            else if (count > 1)
+            {
+                // Get difference between last 2 turns spoken
+                return turns[count - 1] - turns[count - 2];
+            }
+            else
+            {
+                throw new InvalidDataException($"Number {lastNumber} has never been spoken.");
+            }
+
+        }
+    }
+}
--- 2020/AcC2020/Program.cs
+++ 2020/AcC2020/Program.cs
@@ -4,7 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Collections.Generic;
 using AoC.AoC2020.Problems;
-using AoC.AoC2020.Problems.Day14;
+using AoC.AoC2020.Problems.Day15;
 
 namespace AoC.AoC2020
 {
@@ -12,7 +12,7 @@ namespace AoC.AoC2020
     {
         static void Main()
         {
-            var problem = new DockingData();
+            var problem = new RambunctiousRecitation();
             var data = new List<string>();
 
             if (problem.InputFileName != null)
