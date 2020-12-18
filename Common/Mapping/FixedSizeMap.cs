d748f0c761a581d302f0c24bd56ba7fefde0017d
Mark <markpardoe@gmail.com>
2020-12-18 18:59:53 +0000

    Add FixedSizeMap
    Map class for when the search grid is a fixed size (ie. it won't grow).
    This allows us to cache min and max co-oridinates for better performance.

---
+++ Common/Mapping/FixedSizeMap.cs
@@ -0,0 +1,70 @@
+﻿using System.Linq;
+
+namespace AoC.Common.Mapping
+{
+    /// <summary>
+    ///  Map where the area can't grow.
+    /// Min & Max values for X & Y are cached the first time they're accessed
+    /// </summary>
+    /// <typeparam name="TValue"></typeparam>
+    public class FixedSizeMap<TValue> : Map<TValue>
+    {
+        protected int? _maxX;
+        protected int? _minX;
+        protected int? _maxY;
+        protected int? _minY;
+
+        public override int MaxX
+        {
+            get
+            {
+                _maxX ??= Map.Keys.Max(p => p.X);
+                return _maxX.Value;
+            }
+        }
+
+        public override int MinX
+        {
+            get
+            {
+                _minX ??= Map.Keys.Min(p => p.X);
+                return _minX.Value;
+            }
+        }
+
+        public override int MaxY
+        {
+            get
+            {
+                _maxY ??= Map.Keys.Max(p => p.Y);
+                return _maxY.Value;
+            }
+        }
+
+        public override int MinY
+        {
+            get
+            {
+                _minY ??= Map.Keys.Min(p => p.Y);
+                return _minY.Value;
+            }
+        }
+
+
+        public FixedSizeMap(TValue defaultValue, Position topLeft = null, Position bottomRight = null) : base(
+            defaultValue)
+        {
+            if (topLeft != null)
+            {
+                _minY = topLeft.Y;
+                _minX = topLeft.X;
+            }
+
+            if (bottomRight != null)
+            {
+                _maxY = bottomRight.Y;
+                _maxX = bottomRight.X;
+            }
+        }
+    }
+}
