cd861da8b3423a1aa07872c7949455c76dcf1fd7
Mark <markpardoe@gmail.com>
2020-12-19 11:49:14 +0000

    Implement IComparable<MapNode>
    Added helper classes for getting custom IComparers to sort by TotalDistance and DistanceToTarget

--- Common/Mapping/MapNode.cs
+++ Common/Mapping/MapNode.cs
@@ -1,10 +1,11 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Text;
 
 namespace AoC.Common.Mapping
 {
-    public class MapNode : Position, IEquatable<MapNode>
+    public class MapNode : Position, IEquatable<MapNode>, IComparable<MapNode>
     {
         public MapNode Parent { get; set; }
 
@@ -12,6 +13,11 @@ namespace AoC.Common.Mapping
         {
             DistanceFromStart = distanceFromStart;
             Parent = parent;
+        }
+
+        public override string ToString()
+        {
+            return $"[{base.ToString()} FromStart = {DistanceFromStart}, TotalDist = {TotalDistance}]";
         }
 
         public int DistanceFromStart { get; set; } = 0;
@@ -49,6 +55,47 @@ namespace AoC.Common.Mapping
             }
 
             return node;
+        }
+
+        // Sort by total distance
+        public int CompareTo(MapNode other)
+        {
+            if (ReferenceEquals(this, other)) return 0;
+            if (ReferenceEquals(null, other)) return 1;
+
+            return TotalDistance.CompareTo(other.TotalDistance);
+        }
+
+
+        private class SortTotalDistanceAscendingComparer : IComparer<MapNode>
+        {
+
+            public int Compare(MapNode x, MapNode y)
+            {
+                return x.TotalDistance.CompareTo(y.TotalDistance);
+            }
+
+        }
+
+        private class SortDistanceFromStartAscendingComparer : IComparer<MapNode>
+        {
+
+            public int Compare(MapNode x, MapNode y)
+            {
+                return x.DistanceFromStart.CompareTo(y.DistanceFromStart);
+            }
+
+        }
+
+        // Method to return IComparer object for sort helper.
+        public static IComparer<MapNode> SortTotalDistanceAscending()
+        {
+            return new SortTotalDistanceAscendingComparer();
+        }
+        // Method to return IComparer object for sort helper.
+        public static IComparer<MapNode> SortDistanceFromStartAscending()
+        {
+            return new SortDistanceFromStartAscendingComparer();
         }
     }
 }
