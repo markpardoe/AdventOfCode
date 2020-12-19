047a73983177aac933617a31e0b820267ce3d83b
Mark <markpardoe@gmail.com>
2020-12-19 11:49:34 +0000

    Add 2018 day 20 solution

--- 2018/AoC2018/Day20/RegularMap.cs
+++ 2018/AoC2018/Day20/RegularMap.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using AoC.Common;
+using AoC.Common.Mapping;
 
 namespace Aoc.Aoc2018.Day20
 {
@@ -10,18 +11,28 @@ namespace Aoc.Aoc2018.Day20
     {
         public override IEnumerable<int> Solve(IEnumerable<string> input)
         {
-          //  string regEx = input.First();  // shpuld only be one line
-          RoomMap map = new RoomMap(input.First());
-          Console.WriteLine(map.DrawMap());
+            string data = input.First();
+            RoomMap map = new RoomMap(data);
 
 
-            yield return map.CountValue(MapTile.Room);
-            yield return 5;
+            var rooms = map.FindPathToAllTargets();
+            yield return rooms.Max(x => x.DistanceFromStart);
+            yield return rooms.Count(x => x.DistanceFromStart >= 1000);
         }
 
         public override int Year => 2018;
         public override int Day => 20;
         public override string Name => "Day 20: A Regular Map";
         public override string InputFileName => "Day20.txt";
+
+
+        private readonly string _example1 = @"^ENNWSWW(NEWS|)SSSEEN(WNSE|)EE(SWEN|)NNN$";
+        private readonly string _example2 = @"^ESSWWN(E|NNENN(EESS(WNSE|)SSS|WWWSSSSE(SW|NNNE)))$";
+        private readonly string _example3 = @"^WSSEESWWWNW(S|NENNEEEENN(ESSSSW(NWSW|SSEN)|WSWWN(E|WWS(E|SS))))$";
+
+        private readonly string _example4 = @"^E(NN|S)E$"; // 4
+        private readonly string _example5 = @"^(N|S)N$"; // 2
+        private readonly string _example6 = @"^EEE(NN|SSS)EEE$"; // 9
+        private readonly string _example7 = @"^E(N|SS)EEE(E|SSS)$"; // 9
     }
 }
--- 2018/AoC2018/Day20/RoomMap.cs
+++ 2018/AoC2018/Day20/RoomMap.cs
@@ -22,18 +22,19 @@ namespace Aoc.Aoc2018.Day20
     }
     
 
-    public class RoomMap : Map<MapTile>
+    public sealed class RoomMap : Map<MapTile>
     {
         private readonly Position _start = new Position(0,0);
-
+        private readonly HashSet<Position> _endPoints = new HashSet<Position>();
+        
 
         public RoomMap(string input) : base(MapTile.Unknown)
         {
            var inputData = input.Skip(1).ToList();
-
+           Add(_start, MapTile.Room);
            var start = new HashSet<Position>() {_start};
            var data = new Queue<char>(input.ToCharArray());
-           BuildMap(start, data);
+           _endPoints = BuildMap(start, data);  // store endpoints in the maze
 
            // replace unknown with walls
            foreach (var p in GetBoundedEnumerator())
@@ -44,8 +45,35 @@ namespace Aoc.Aoc2018.Day20
                }
            }
         }
-
         
+        public int FindMaxDoorsToEndPoint()
+        {
+            int maxDistance = 0;
+            //foreach (var endPoint in _endPoints)
+            //{
+            //    Console.WriteLine($"Finding Target {endPoint} of {_endPoints.Count}");
+            //    var node = FindPathTo(_start, endPoint);
+            //    if (node != null && node.DistanceFromStart > maxDistance)
+            //    {
+            //        maxDistance = node.DistanceFromStart;
+            //    }
+
+            //    Console.WriteLine($"Target {endPoint} Found: {node.DistanceFromStart}");
+            //}
+
+            var paths = PathToTargets(_start, _endPoints);
+            return paths.Max(x => x.DistanceFromStart);
+
+        }
+
+        public HashSet<MapNode> FindPathToAllTargets()
+        {
+            var rooms = Map.Where(x => x.Value == MapTile.Room).Select(x => x.Key);
+            var result = PathToTargets(_start, rooms);
+
+            return result;
+        }
+
         public HashSet<Position> BuildMap(HashSet<Position> startPositions, Queue<char> inputData)
         {
             // Where we end up at the end each path
@@ -71,7 +99,6 @@ namespace Aoc.Aoc2018.Day20
 
                 else if (token == ')') // End of a group of directions
                 {
-                    finalPositions.UnionWith(currentPositions);
                     break;
                 }
 
@@ -88,7 +115,7 @@ namespace Aoc.Aoc2018.Day20
                 }
                 else if (token == '^' || token == '$')
                 {
-                    // ignore as this indicates start or end of RegEx
+                    // ignore as this indicates start or end of input
                     continue;
                 }
                 else
@@ -97,9 +124,142 @@ namespace Aoc.Aoc2018.Day20
                 }
             }
 
+            finalPositions.UnionWith(currentPositions);
             return finalPositions;
         }
-        
+
+        /// <summary>
+        /// Performs a BFS to find shortest path to every target in the input
+        /// Assumes an unweighted map (ie. distance/cost between points always = 1).
+        /// </summary>
+        /// <param name="targets"></param>
+        /// <returns></returns>
+        public HashSet<MapNode> PathToTargets(Position start, IEnumerable<Position> targets)
+        {
+            // Create a copy of targets to avoid modifying in place
+            HashSet<Position> targetList = new HashSet<Position>(targets);
+            HashSet<MapNode> output = new HashSet<MapNode>();
+
+            Queue<MapNode> openList = new Queue<MapNode>();
+            HashSet<MapNode> closedList = new HashSet<MapNode>();
+
+            openList.Enqueue(new MapNode(start));
+
+            while (openList.Count > 0)
+            {
+                // get the 1st node
+                var currentPosition = openList.Dequeue();
+
+                var neighbors = GetAvailableNeighbors(currentPosition)
+                    .Select(x => new MapNode(x, currentPosition, currentPosition.DistanceFromStart + 1));
+
+                foreach (var n in neighbors)
+                {
+                    // We've reached a target - BFS guarentees its the shortest path
+                    if (targetList.Contains(currentPosition))
+                    {
+                        output.Add(currentPosition);
+                        targetList.Remove(currentPosition);
+
+                        // found all the targets - so quit
+                        if (targetList.Count == 0)
+                        {
+                            return output;
+                        }
+                    }
+
+                    // Check if we've already visited the node (or about to visit it)
+                    // BFS guarantee shortest-path, so we don't have to check if the new distance is shorter
+                    if (closedList.Contains(n) || openList.Contains(n))
+                    {
+                        // Already got a shorter path to this position
+                        continue;
+                    }
+                    openList.Enqueue(n);
+                }
+                closedList.Add(currentPosition);
+            }
+            return output;
+        }
+
+        /// <summary>
+        /// Finds the sghortest path from start to target positions using A* search algorithm
+        /// https://www.geeksforgeeks.org/a-search-algorithm/
+        /// </summary>
+        /// <param name="start"></param>
+        /// <param name="target"></param>
+        public MapNode FindPathTo(Position start, Position target)
+        {
+            HashSet<MapNode> openList = new HashSet<MapNode>() {new MapNode(start)};
+            HashSet<MapNode> closedList = new HashSet<MapNode>();
+
+            while (openList.Count > 0)
+            {
+                // get the node with the shortest distance
+                var currentPosition = openList.OrderBy(x => x.TotalDistance).First();
+                openList.Remove(currentPosition);
+
+                // Console.WriteLine($"Checking {currentPosition}");
+
+                var neighbors = GetAvailableNeighbors(currentPosition)
+                    .Select(x => new MapNode(x, currentPosition, currentPosition.DistanceFromStart + 1)).ToList();
+                
+                foreach (var n in neighbors)
+                {
+                    n.CalculatedDistanceToTarget = n.DistanceTo(target);
+                    if (n.X == target.X && n.Y == target.Y)
+                    {
+                        return n;
+                    }
+
+                    // Check if node already in open list with a lower TotalDistance
+                    if (openList.Any(x => x.X == n.X && x.Y == n.Y && x.TotalDistance <= n.TotalDistance))
+                    {
+                        // Already got a shorter path to this position
+                        continue;
+                    }
+
+                    // Check if node with same position in closedList with lower distance (ie. we've already found a faster way to this position)
+                    // Check if node already in open list with a lower TotalDistance
+                    if (closedList.Any(x => x.X == n.X && x.Y == n.Y && x.TotalDistance <= n.TotalDistance))
+                    {
+                        // Already got a shorter path to this position
+                        continue;
+                    }
+
+                    openList.Add(n);
+                }
+
+                closedList.Add(currentPosition);
+            }
+
+            return null; // no destination found
+        }
+
+        /// <summary>
+        /// Returns neighboring rooms.
+        /// We could return the poistion of the doors (as they are technically adjourning)
+        /// but faster to skip search step of a position that will only ever have one exit.
+        /// </summary>
+        /// <param name="position"></param>
+        /// <returns></returns>
+        public override IEnumerable<Position> GetAvailableNeighbors(Position position)
+        {
+            var result = new HashSet<Position>();
+            if (this[position] != MapTile.Room) return result; 
+
+            // Get neighboring doors
+            var neighbors = position.GetNeighboringPositions()
+                .Where(x => this[x] == MapTile.DoorHoziontal || this[x] == MapTile.DoorVertical);
+
+            // We don't want the location of the door - we want the room AFTER the door.  So move another square in that direction
+            foreach (var door in neighbors)
+            {
+                Direction direction = position.FindDirection(door);
+                result.Add(door.Move(direction));
+            }
+            return result;
+        }
 
         // easy way to tell if its a move command, or a special character
         private readonly Dictionary<char, CompassDirection> _validDirections = new Dictionary<char, CompassDirection>()
