40d4d3006c0d85b658dc52bc94516ff180c8997c
Mark <markpardoe@gmail.com>
2020-12-20 12:54:32 +0000

    Move CompassDirection into AoC2018 solution

---
+++ 2018/AoC2018/Common/CompassDirection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Aoc.Aoc2018.Common
+{
+    /// <summary>
+    ///  Holds compass directions - some problems give inputs as N, S, E or W
+    /// </summary>
+    public enum CompassDirection
+    {
+        North = 'N',
+        South = 'S',
+        West = 'W',
+        East = 'E'
+    }
+}
