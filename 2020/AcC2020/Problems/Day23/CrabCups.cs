358c23d97bdc3ac4b5d4a82e49e7bd2a4cc2507d
Mark <markpardoe@gmail.com>
2020-12-23 08:06:48 +0000

    Rewrite of day 23 to use HashedLinkedList instead of array & Linkedlist
    About 33% slower - but simpler to understand

--- 2020/AcC2020/Problems/Day23/CrabCups.cs
+++ 2020/AcC2020/Problems/Day23/CrabCups.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using AoC.Common;
 
 namespace AoC.AoC2020.Problems.Day23
@@ -16,188 +15,23 @@ namespace AoC.AoC2020.Problems.Day23
 
         public override IEnumerable<long> Solve(IEnumerable<string> input)
         {
-            CrabGame game = new CrabGame(puzzleInput);
-            RunGame(game, 100);
-            yield return game.CupOrderValue();
+            string gameInput = puzzleInput;
 
-            CrabGame game2 = new CrabGame(puzzleInput, 1000000);
-            RunGame(game2, 10000000);
-            yield return game2.GetCalculatedValue();
-
+            yield return RunGame(gameInput, gameInput.Length, 100).CupOrderValue();
+            yield return RunGame(gameInput, 1000000, 10000000).GetCalculatedValue();
         }
 
         private readonly string puzzleInput = "253149867";
 
-        private void RunGame(CrabGame game, int numTurns)
+        private CrabGame RunGame(string input, int numCups, int numTurns)
         {
+            var game = new CrabGame(input, numCups);
             for (int i = 0; i < numTurns; i++)
             {
                 game.Move();
             }
-        }
-    }
 
-    public class CrabGame
-    {
-        private readonly LinkedList<int> _gameData = new LinkedList<int>();
-      //  private readonly Dictionary<int, LinkedListNode<int>> _valueLookup = new Dictionary<int,LinkedListNode<int>>();
-       private readonly LinkedListNode<int>[] _nodes;
-       //private readonly int HighestValue;
-       // private readonly int LowestValue;
-        private int _move = 1;
-        
-        private LinkedListNode<int> _current;
-
-        public int CupOrderValue()
-        {
-            var sb = new StringBuilder();
-            var node = _nodes[1];
-            node = node.Next ?? _gameData.First;
-
-            while (node.Value != 1)
-            {
-                sb.Append(node.Value);
-                node = node.Next ?? _gameData.First;
-            }
-
-            return int.Parse(sb.ToString());
-        }
-
-        public long GetCalculatedValue()
-        {
-            var node = _nodes[1];
-            node = node.Next ?? _gameData.First;
-
-            long total = node.Value;
-            node = node.Next ?? _gameData.First;
-
-            return total * node.Value;
-        }
-
-        public override string ToString()
-        {
-            StringBuilder sb = new StringBuilder();
-            foreach (var node in _gameData)
-            {
-                if (node == _current.Value)
-                {
-                    sb.Append($" ({node}) ");
-                }
-                else
-                {
-                    sb.Append($"  {node}  ");
-                }
-            }
-
-            return sb.ToString();
-        }
-
-        public CrabGame(string rawData, int numCups = 0)
-        {
-            int maxValue = 0;
-
-            int length = numCups;
-            if (numCups < rawData.Length)
-            {
-                length = rawData.Length;
-            }
-            // Create array to cache lookups per value
-            _nodes = new LinkedListNode<int>[length + 1];
-
-            foreach (var c in rawData)
-            {
-                int value = int.Parse(c.ToString());
-                var node = _gameData.AddLast(value);
-                _nodes[value] = node;
-
-                if (value > maxValue)
-                {
-                    maxValue = value;
-                }
-            }
-
-            // Add remaining cups
-            if (numCups > _gameData.Count)
-            {
-                int num = maxValue + 1;
-                while (_gameData.Count < numCups)
-                {
-                    var node = _gameData.AddLast(num);
-                    _nodes[num] = node;
-                    num++;
-                }
-            }
-
-            _current = _gameData.First;
-        }
-
-        public void Move()
-        {
-            //Console.Out.WriteLine($"-- move {_move} --");
-            //Console.WriteLine(this.ToString());
-
-            var removedCups = new List<int>();
-
-            // remove next 3 cups
-            for (int i = 0; i < 3; i++)
-            {
-                var cup = _current.Next ?? _gameData.First;
-                removedCups.Add(cup.Value);
-                Remove(cup);
-            }
-
-            //Console.WriteLine($"pick up: {string.Join(", ",removedCups)}");
-
-            LinkedListNode<int> destination = FindNode(_current.Value -1);
-            //Console.WriteLine($"Destination: {destination.Value}");
-
-            // add removed cups
-            AddNodes(destination, removedCups);
-
-            _current = _current.Next ?? _gameData.First;
-            _move++;
-            //Console.Out.WriteLine();
-        }
-
-        // Find the node with given value.
-        // If it doesn't exist - subtract one and try again.
-        private LinkedListNode<int> FindNode(int value)
-        {
-            while (true)
-            {
-                // loop around
-                if (value < 0)
-                {
-                    value = _nodes.Length-1;
-                }
-
-                if (_nodes[value] != null)
-                {
-                   return _nodes[value];
-                }
-                else
-                {
-                    value--;
-                }
-            }
-        }
-
-        private void Remove(LinkedListNode<int> node)
-        {
-            _gameData.Remove(node);
-            _nodes[node.Value] = null;
-        }
-
-        private void AddNodes(LinkedListNode<int> node, List<int> cups)
-        {
-            // Add cups in reverse order as each new node will be inserted after the current node 
-            // pushing the others along 1 space
-            for (int i = cups.Count - 1; i >= 0;i--)
-            {
-                int value = cups[i];
-                var newNode = _gameData.AddAfter(node, value);
-                _nodes[value] = newNode;
-            }
+            return game;
         }
     }
 }
---
+++ 2020/AcC2020/Problems/Day23/CrabGame.cs
@@ -0,0 +1,151 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AoC.Common.DataStructures;
+
+namespace AoC.AoC2020.Problems.Day23
+{
+    public class CrabGame
+    {
+        //private readonly LinkedList<int> _gameData = new LinkedList<int>();
+        //private readonly LinkedListNode<int>[] _nodes;
+        private readonly HashedLinkedList<int> _gameData = new HashedLinkedList<int>();
+        private int _move = 1;
+        
+        private LinkedListNode<int> _current;
+
+        public int CupOrderValue()
+        {
+            var sb = new StringBuilder();
+            var node = _gameData[1];
+            node = node.Next ?? _gameData.First;
+
+            while (node.Value != 1)
+            {
+                sb.Append(node.Value);
+                node = node.Next ?? _gameData.First;
+            }
+
+            return int.Parse(sb.ToString());
+        }
+
+        public long GetCalculatedValue()
+        {
+            var node = _gameData[1];
+            node = node.Next ?? _gameData.First;
+
+            long total = node.Value;
+            node = node.Next ?? _gameData.First;
+
+            return total * node.Value;
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new StringBuilder();
+            foreach (var node in _gameData)
+            {
+                if (node == _current.Value)
+                {
+                    sb.Append($" ({node}) ");
+                }
+                else
+                {
+                    sb.Append($"  {node}  ");
+                }
+            }
+
+            return sb.ToString();
+        }
+
+        public CrabGame(string rawData, int numCups = 0)
+        {
+            int maxValue = 0;
+
+            int length = numCups;
+            if (numCups < rawData.Length)
+            {
+                length = rawData.Length;
+            }
+
+            foreach (var c in rawData)
+            {
+                int value = int.Parse(c.ToString());
+                var node = _gameData.AddLast(value);
+
+                if (value > maxValue)
+                {
+                    maxValue = value;
+                }
+            }
+
+            // Add remaining cups
+            if (numCups > _gameData.Count)
+            {
+                int num = maxValue + 1;
+                while (_gameData.Count < numCups)
+                {
+                    var node = _gameData.AddLast(num);
+                    num++;
+                }
+            }
+
+            _current = _gameData.First;
+        }
+
+        public void Move()
+        {
+            var removedCups = new List<int>();
+
+            // remove next 3 cups
+            for (int i = 0; i < 3; i++)
+            {
+                var cup = _current.Next ?? _gameData.First;
+                removedCups.Add(cup.Value);
+                _gameData.Remove(cup);
+            }
+
+            LinkedListNode<int> destination = FindNode(_current.Value -1);
+
+            // add removed cups
+            AddNodes(destination, removedCups);
+
+            _current = _current.Next ?? _gameData.First;
+            _move++;
+        }
+
+        // Find the node with given value.
+        // If it doesn't exist - subtract one and try again.
+        // in theory every number should exist in the gameData - so should never need to loop more than once
+        private LinkedListNode<int> FindNode(int value)
+        {
+            while (true)
+            {
+                // loop around
+                if (value < 0)
+                {
+                    value = _gameData.Keys.Max();
+                }
+
+                if (_gameData.Contains(value))
+                {
+                    return _gameData[value];
+                }
+                else
+                {
+                    value--;
+                }
+            }
+        }
+
+        private void AddNodes(LinkedListNode<int> node, List<int> cups)
+        {
+            // Add cups in reverse order as each new node will be inserted after the current node 
+            // pushing the others along 1 space
+            for (int i = cups.Count - 1; i >= 0;i--)
+            {
+                _gameData.AddAfter(node, cups[i]);
+            }
+        }
+    }
+}
