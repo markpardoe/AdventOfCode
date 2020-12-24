5e8efe7e2a575fa8b0b26314f308984bed5ea737
Mark <markpardoe@gmail.com>
2020-12-24 10:39:01 +0000

    Add 2017 solutions project

---
+++ 2017/AoC2017/Day01/InverseCaptcha.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using AoC.Common;
+
+namespace AoC2017.Day01
+{
+    public class InverseCaptcha :AoCSolution<int>
+    {
+        public override int Year => 2017;
+        public override int Day => 1;
+        public override string Name => "Day 1: Inverse Captcha";
+        public override string InputFileName => "Day01.txt";
+
+        public override IEnumerable<int> Solve(IEnumerable<string> input)
+        {
+            var captcha = input.First().Select(x => x - 48).ToList();  // only one line 
+            yield return SolveA(captcha);
+            yield return SolveB(captcha);
+
+        }
+
+        private int SolveA(List<int> captcha)
+        {
+            int sum = 0;
+            for (int i = 0; i < captcha.Count() - 1; i++)
+            {
+                if (captcha[i] == captcha[i + 1])
+                {
+                    sum += captcha[i];
+                }
+            }
+
+            // check if first = last
+            if (captcha.Last() == captcha.First())
+            {
+                sum += captcha.First();
+            }
+
+            return sum;
+        }
+
+        private int SolveB(List<int> captcha)
+        {
+            int sum = 0;
+            int mod = captcha.Count / 2;
+            for (int i = 0; i < captcha.Count(); i++)
+            {
+                int index = (i + mod) % captcha.Count;
+                if (captcha[i] == captcha[index])
+                {
+                    sum += captcha[i];
+                }
+            }
+
+           
+
+            return sum;
+        }
+    }
+}
---
+++ 2017/AoC2017/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace AoC2017
+{
+    public class Program
+    {
+        static void Main()
+        {
+            var problem = new Day01.InverseCaptcha();
+            var data = new List<string>();
+
+            if (problem.InputFileName != null)
+            {
+                string inputFile = Path.Combine(System.Environment.CurrentDirectory, "InputData", problem.InputFileName);
+                data.AddRange(File.ReadAllLines(inputFile).ToList());
+            }
+
+            Console.WriteLine(problem.Name);
+
+            // Time problem execution - ignoring time for reading input file
+            Stopwatch stopWatch = new Stopwatch();
+            stopWatch.Start();
+            foreach (var result in problem.Solve(data))
+            {
+                Console.WriteLine(result);
+            }
+            stopWatch.Stop();
+            Console.WriteLine();
+            Console.WriteLine($"Solved in {stopWatch.ElapsedMilliseconds}ms");
+            Console.WriteLine("Press enter to exit.");
+            Console.ReadLine();
+        }
+    }
+}
